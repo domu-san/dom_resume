7b309adff3701ba23bac04efb7777ccff13e4abb
Dominic Suico <domsuico@gmail.com>
2020-09-07 23:15:53 +0800

    update

--- Personal Projects/Dice Game/index.js
+++ Personal Projects/Dice Game/index.js
@@ -1,21 +1,18 @@
-function rst(){
-	document.location.reload(true);
+function rst() {
+    document.location.reload(true);
 }
 
-function diceGame() {
+function mainFunc() {
     var nameOne = document.getElementById("nameOne").value;
-    var nameTwo = document.getElementById("nameTwo").value;
-
-    var randNum2 = Math.floor((Math.random() * 6) + 1);
     var randNum1 = Math.floor((Math.random() * 6) + 1);
-
     var diceImageP1 = "p1_" + randNum1 + ".png";
-    var diceImageP2 = "p2_" + randNum2 + ".png";
-
     var diceImageSrc1 = "img/" + diceImageP1;
-    var diceImageSrc2 = "img/" + diceImageP2;
-    
     document.getElementById("dice1").setAttribute("src", diceImageSrc1);
+    
+    var nameTwo = document.getElementById("nameTwo").value;
+    var randNum2 = Math.floor((Math.random() * 6) + 1);
+    var diceImageP2 = "p2_" + randNum2 + ".png";
+    var diceImageSrc2 = "img/" + diceImageP2;
     document.getElementById("dice2").setAttribute("src", diceImageSrc2);
 
     if (randNum1 > randNum2) {
@@ -24,10 +21,11 @@ function diceGame() {
         document.getElementById("hdr").innerHTML = nameTwo + " wins";
 
     } else {
-    	document.getElementById("hdr").innerHTML ="It's a tie! Roll Again";
+        document.getElementById("hdr").innerHTML = "It's a tie! Roll Again";
     }
 }
 
-    document.getElementById("btn").addEventListener("click", diceGame);
 
-    document.getElementById("rst").addEventListener("click", rst);
+document.getElementById("btn").addEventListener("click", mainFunc);
+
+document.getElementById("rst").addEventListener("click", rst);
